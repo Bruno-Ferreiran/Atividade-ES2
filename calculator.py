476faab570895890a4695525eb98729c7ff713d2
unknown <ryanbettin44@gmail.com>
2024-04-25 01:28:37 -0300

    calculo

--- calculator.py
+++ calculator.py
@@ -14,3 +14,16 @@ while True:
 
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+    elif choice == '5':
+        print(num1, "^", num2, "=", power(num1, num2))
+    else:
+        print("Entrada inválida")
